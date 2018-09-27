ce03e7f7f59ae35a79a027371b40c435b260348d
Robert Dennis <rdennis@doctorlogic.com>
2018-09-27 12:43:33 -0500

    Removed webpack, we don't need it!

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
 import * as React from 'react';
 export declare function Renderer(COMPONENTS: {
     [key: string]: React.ComponentClass;
-}): (cb: (err: any, html: string) => void, command: "component" | "tree", ...args: any[]) => void;
+}): (cb: (err: any, html: string) => void, command: "tree" | "component", ...args: any[]) => void;
